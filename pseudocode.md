79bc3213e50599008002063c9491d5127ccda2eb
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-02-05 17:13:37 +0000

    completed precise array search for location details

---
+++ pseudocode.md
@@ -0,0 +1,4 @@
+
+getting the serch in put by locationg -
+*set a variable to collect the user input 
+*use that variable to add it to the url from geocoding 
--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,35 @@
 // Add your own API key between the ""
 //var APIKey = "e10da35d7020e3de6262203a6622a4be";
+// 5day = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e10da35d7020e3de6262203a6622a4be
 
-let searchInputEl = document.querySelector("#search-input");
+// let searchInputEl = document.querySelector("#search-input");
 
 
 //giving functionality to the search button 
 $("#search-button").on("click", function(event) {
   event.preventDefault();
-  
+
+  // getting search input
+  let searchInputEl = document.querySelector("#search-input");
+  let cityName = searchInputEl.value;
 // Here we are building the URL we need to query the database
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e10da35d7020e3de6262203a6622a4be"
+var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=e10da35d7020e3de6262203a6622a4be`
   //
   // We then created an AJAX call
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  console.log(response);
-});
+  let lat = response[0].lat; 
+  let lon = response[0].lon;
+
+  $.ajax({
+    url : `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e10da35d7020e3de6262203a6622a4be`,
+    method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      
     });
+})
 
-
-
+})
